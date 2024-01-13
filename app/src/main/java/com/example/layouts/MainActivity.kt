d5a4fe83ba3b5838783a08848aaf9e330f7a75b1
Ilya <allonblack66@gmail.com>
2024-01-13 23:31:03 +0100

    Homework №11

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -15,23 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val x1  = 0.0F
-//        val x2 = 0.0F
-//        val y1 = 0.0F
-//        val y2 = 0.0F
-//        val start = getColor(R.color.bgColorPrimary)
-//        val end = getColor(R.color.bgColorSecondary)
-//        var bgColor: LinearGradient = LinearGradient(x1, y1, x2, y2, start, end, Shader.TileMode.CLAMP)
-//        val bg = findViewById<LinearLayout>(R.id.background)
-//       bgColor.setLocalMatrix(rotationMatrix(0f))
-
-
-
-       // bg.setBackgroundColor(bgColor.toString().toInt())
-
-
-
-
     }
 
 
