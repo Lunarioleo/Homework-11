074bc6643d96929f16f529f56e34df7d8f479479
Ilya <allonblack66@gmail.com>
2024-02-29 15:32:16 +0100

    Homework 24

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -1,23 +1,53 @@
 package com.example.layouts
 
 
-import android.graphics.Color
-import android.graphics.LinearGradient
-import android.graphics.Matrix
-import android.graphics.Shader
+
+import android.animation.ObjectAnimator
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
-import androidx.core.graphics.rotationMatrix
+import android.view.View
+
+import com.example.layouts.databinding.ActivityMainBinding
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+
+    GlobalScope.launch(Dispatchers.Main) {
+        binding.startAnimation.playAnimation()
+        binding.startAnimation.repeatCount = 2
+        delay(3000)
+        binding.startAnimation.visibility = View.INVISIBLE
+        binding.secondaryDiv.visibility = View.VISIBLE
+        animationSlideUp(binding.secondaryDiv, 1000)
+        animationFadeIn(binding.secondaryDiv, 1000)
+        animationFadeIn(binding.image, 1500)
+    }
     }
 
+     fun animationFadeIn(view: View,   d: Long){
+        val a  = ObjectAnimator.ofFloat(view, View.ALPHA, 0.0f, 1.0f)
+        a.duration = d
+        a.start()
+    }
 
-
-
+     fun animationSlideUp(view: View, d: Long){
+        val a  = ObjectAnimator.ofFloat(view, View.TRANSLATION_X, -700f, 0f)
+        a.duration = 500
+        a.start()
+    }
 }
+
+
